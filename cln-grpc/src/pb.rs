b9083599bd51ed075e1f04a66e189a03e85d4020
Riccardo Casatta <riccardo@casatta.it>
2023-01-13 10:33:09 +0100

    cln-grpc: update listpeers json fixing tests

--- cln-grpc/src/pb.rs
+++ cln-grpc/src/pb.rs
@@ -190,7 +190,9 @@ mod test {
                   "funding": {
                     "local_msat": "0msat",
                     "remote_msat": "1000000000msat",
-                    "pushed_msat": "0msat"
+                    "pushed_msat": "0msat",
+                    "local_funds_msat": "0msat",
+                    "remote_funds_msat": "0msat",
                   },
                   "msatoshi_to_us": 0,
                   "to_us_msat": "0msat",
@@ -284,12 +286,14 @@ mod test {
                   "opener": "local",
                   "features": [
                     "option_static_remotekey",
-                    "option_anchor_outputs"
+                    "option_anchor_outputs",
                   ],
                   "funding": {
                     "local_msat": "1000000000msat",
                     "remote_msat": "0msat",
-                    "pushed_msat": "0msat"
+                    "pushed_msat": "0msat",
+                    "local_funds_msat": "0msat",
+                    "remote_funds_msat": "0msat"
                   },
                   "msatoshi_to_us": 1000000000,
                   "to_us_msat": "1000000000msat",
